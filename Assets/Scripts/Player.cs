7fb6fc3235e8ac225ab3f7a46e7e3546a4a478fc
Zephilinox <Zephilinox@hotmail.co.uk>
2019-10-05 10:45:53 +0100

    Player.cs

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Vector2 moveDirection;
+    float moveSpeed = 2;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        moveDirection = Vector2.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            moveDirection.y += 1;
+        }
+        
+        if (Input.GetKey(KeyCode.A))
+        {
+            moveDirection.x -= 1;
+        }
+        
+        if (Input.GetKey(KeyCode.S))
+        {
+            moveDirection.y -= 1;
+        }
+        
+        if (Input.GetKey(KeyCode.D))
+        {
+            moveDirection.x += 1;
+        }
+
+        Vector2 movement = moveDirection * moveSpeed * Time.deltaTime;
+        transform.Translate(movement, Space.World);
+    }
+
+    private void FixedUpdate()
+    {
+    }
+}
