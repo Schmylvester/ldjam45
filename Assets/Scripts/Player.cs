0da165f542df0895a5458519c6bc8ebba8db0d7c
Thomas <tomsylvester1@yahoo.co.uk>
2019-10-05 12:38:58 +0100

    Player namespace

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,118 +2,121 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+namespace Player
 {
-    struct PerFrame
+    public class Player : MonoBehaviour
     {
-        public Vector2 moveDirection;
-        public bool interacted;
-    };
-
-    PerFrame lastFrame;
-    PerFrame thisFrame;
-
-    float moveSpeed = 100;
-
-    void Start()
-    {
-        lastFrame.moveDirection = Vector2.zero;
-        lastFrame.interacted = false;
-        thisFrame.moveDirection = Vector2.zero;
-        thisFrame.interacted = false;
-
-        GetComponentInChildren<AnimationStateController>().SetState("IdleDown");
-    }
-
-    void HandleInput()
-    {
-        lastFrame.moveDirection = thisFrame.moveDirection;
-        thisFrame.moveDirection = Vector2.zero;
-
-        if (Input.GetKey(KeyCode.W))
+        struct PerFrame
         {
-            thisFrame.moveDirection.y += 1;
+            public Vector2 moveDirection;
+            public bool interacted;
+        };
+
+        PerFrame lastFrame;
+        PerFrame thisFrame;
+
+        float moveSpeed = 100;
+
+        void Start()
+        {
+            lastFrame.moveDirection = Vector2.zero;
+            lastFrame.interacted = false;
+            thisFrame.moveDirection = Vector2.zero;
+            thisFrame.interacted = false;
+
+            GetComponentInChildren<AnimationStateController>().SetState("IdleDown");
         }
 
-        if (Input.GetKey(KeyCode.A))
+        void HandleInput()
         {
-            thisFrame.moveDirection.x -= 1;
-        }
+            lastFrame.moveDirection = thisFrame.moveDirection;
+            thisFrame.moveDirection = Vector2.zero;
 
-        if (Input.GetKey(KeyCode.S))
-        {
-            thisFrame.moveDirection.y -= 1;
-        }
-
-        if (Input.GetKey(KeyCode.D))
-        {
-            thisFrame.moveDirection.x += 1;
-        }
-
-        if (Input.GetKey(KeyCode.Space))
-        {
-            thisFrame.interacted = true;
-        }
-    }
-
-    void Move()
-    {
-        Vector2 movement = thisFrame.moveDirection * moveSpeed * Time.fixedDeltaTime;
-        GetComponent<Rigidbody2D>().velocity = movement;
-    }
-
-    void Animate()
-    {
-        if (lastFrame.moveDirection.y != 1 &&
-            thisFrame.moveDirection.y == 1)
-        {
-            GetComponentInChildren<AnimationStateController>().SetState("WalkUp");
-        }
-        else if (lastFrame.moveDirection.y != -1 && 
-            thisFrame.moveDirection.y == -1)
-        {
-            GetComponentInChildren<AnimationStateController>().SetState("WalkDown");
-        }
-        else if (lastFrame.moveDirection.x != -1 &&
-            thisFrame.moveDirection.x == -1)
-        {
-            GetComponentInChildren<AnimationStateController>().SetState("WalkLeft");
-        }
-        else if (lastFrame.moveDirection.x != 1 &&
-            thisFrame.moveDirection.x == 1)
-        {
-            GetComponentInChildren<AnimationStateController>().SetState("WalkRight");
-        }
-
-        if (thisFrame.moveDirection == Vector2.zero)
-        {
-            if (lastFrame.moveDirection.x == -1)
+            if (Input.GetKey(KeyCode.W))
             {
-                GetComponentInChildren<AnimationStateController>().SetState("IdleLeft");
+                thisFrame.moveDirection.y += 1;
             }
-            else if (lastFrame.moveDirection.x == 1)
+
+            if (Input.GetKey(KeyCode.A))
             {
-                GetComponentInChildren<AnimationStateController>().SetState("IdleRight");
+                thisFrame.moveDirection.x -= 1;
             }
-            else if (lastFrame.moveDirection.y == -1)
+
+            if (Input.GetKey(KeyCode.S))
             {
-                GetComponentInChildren<AnimationStateController>().SetState("IdleDown");
+                thisFrame.moveDirection.y -= 1;
             }
-            else if (lastFrame.moveDirection.y == 1)
+
+            if (Input.GetKey(KeyCode.D))
             {
-                GetComponentInChildren<AnimationStateController>().SetState("IdleUp");
+                thisFrame.moveDirection.x += 1;
+            }
+
+            if (Input.GetKey(KeyCode.Space))
+            {
+                thisFrame.interacted = true;
             }
         }
-    }
 
-    void Update()
-    {
-        HandleInput();
-        Animate();
-    }
+        void Move()
+        {
+            Vector2 movement = thisFrame.moveDirection * moveSpeed * Time.fixedDeltaTime;
+            GetComponent<Rigidbody2D>().velocity = movement;
+        }
 
-    private void FixedUpdate()
-    {
-        Move();
+        void Animate()
+        {
+            if (lastFrame.moveDirection.y != 1 &&
+                thisFrame.moveDirection.y == 1)
+            {
+                GetComponentInChildren<AnimationStateController>().SetState("WalkUp");
+            }
+            else if (lastFrame.moveDirection.y != -1 && 
+                thisFrame.moveDirection.y == -1)
+            {
+                GetComponentInChildren<AnimationStateController>().SetState("WalkDown");
+            }
+            else if (lastFrame.moveDirection.x != -1 &&
+                thisFrame.moveDirection.x == -1)
+            {
+                GetComponentInChildren<AnimationStateController>().SetState("WalkLeft");
+            }
+            else if (lastFrame.moveDirection.x != 1 &&
+                thisFrame.moveDirection.x == 1)
+            {
+                GetComponentInChildren<AnimationStateController>().SetState("WalkRight");
+            }
+
+            if (thisFrame.moveDirection == Vector2.zero)
+            {
+                if (lastFrame.moveDirection.x == -1)
+                {
+                    GetComponentInChildren<AnimationStateController>().SetState("IdleLeft");
+                }
+                else if (lastFrame.moveDirection.x == 1)
+                {
+                    GetComponentInChildren<AnimationStateController>().SetState("IdleRight");
+                }
+                else if (lastFrame.moveDirection.y == -1)
+                {
+                    GetComponentInChildren<AnimationStateController>().SetState("IdleDown");
+                }
+                else if (lastFrame.moveDirection.y == 1)
+                {
+                    GetComponentInChildren<AnimationStateController>().SetState("IdleUp");
+                }
+            }
+        }
+
+        void Update()
+        {
+            HandleInput();
+            Animate();
+        }
+
+        private void FixedUpdate()
+        {
+            Move();
+        }
     }
 }
