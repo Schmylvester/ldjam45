270cff33afe57e18bf31fe3ddb4653e1e27c1476
Thomas <tomsylvester1@yahoo.co.uk>
2019-10-05 01:02:50 +0100

    Got an audio manager and some nice sounds to play2

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game
+{
+    public class AudioManager : MonoBehaviour
+    {
+        [System.Serializable]
+        public struct AudioBGM
+        {
+            public string id;
+            public AudioClip intro;
+            public AudioClip loop;
+        }
+
+        [SerializeField] private AudioBGM[] m_tracks;
+        [SerializeField] private AudioSource m_audioSource;
+        int m_activeTrackIdx = -1;
+
+        public void playMusic(int idx)
+        {
+            m_activeTrackIdx = idx;
+            float introLength = 0;
+            if (m_tracks[idx].intro)
+            {
+                m_audioSource.clip = m_tracks[idx].intro;
+                introLength = m_tracks[idx].intro.length;
+                m_audioSource.Play();
+            }
+            StartCoroutine(introEnd(introLength));
+        }
+
+        public void playMusic(string id)
+        {
+            for(int i = 0; i < m_tracks.Length; ++i)
+            {
+                if(m_tracks[i].id == id)
+                {
+                    playMusic(i);
+                }
+            }
+        }
+
+        private IEnumerator introEnd(float time)
+        {
+            yield return new WaitForSeconds(time);
+            m_audioSource.clip = m_tracks[m_activeTrackIdx].loop;
+            m_audioSource.loop = true;
+            m_audioSource.Play();
+        }
+
+        private void Update()
+        {
+            for(int i = 49; i < 57; ++i)
+            {
+                if(Input.GetKeyDown((KeyCode)i))
+                {
+                    playMusic(i - 49);
+                }
+            }
+        }
+    }
+}
