ec76b36d610a23080333685d9c50f360d93e8432
Zephilinox <Zephilinox@hotmail.co.uk>
2019-10-06 10:14:57 +0100

    PlayerHP

---
+++ Assets/Scripts/PlayerHPBar.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHPBar : MonoBehaviour
+{
+    PlayerStats stats;
+
+    float originalWidth = 0;
+    private void Start()
+    {
+        stats = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerStats>();
+        originalWidth = GetComponent<RectTransform>().rect.width;
+    }
+
+    private void Update()
+    {
+        float playerHpPercent = 0;
+        if (stats.currentHealth > 0)
+        {
+            playerHpPercent = stats.currentHealth / stats.GetActualMaxHealth();
+        }
+
+        Vector3 scale = GetComponent<RectTransform>().transform.localScale;
+        scale.x = playerHpPercent;
+        GetComponent<RectTransform>().transform.localScale = scale;
+    }
+}
