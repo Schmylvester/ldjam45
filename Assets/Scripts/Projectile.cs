7c79864a5fd0421cc2cf3863f585f5fde44c479c
Zephilinox <Zephilinox@hotmail.co.uk>
2019-10-06 18:24:42 +0100

    Fixed chicken, added projectile, added ranged weapon, monster charge

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public Vector2 direction;
+    public float speed;
+    public float damage;
+    public float range;
+    public bool firedByPlayer;
+
+    Vector2 spawnPos;
+    private void Start()
+    {
+        spawnPos = transform.position;
+        GetComponent<Rigidbody2D>().transform.LookAt(Vector3.forward, direction);
+        Destroy(gameObject, 10);
+    }
+
+    private void FixedUpdate()
+    {
+        GetComponent<Rigidbody2D>().velocity = direction * speed * Time.fixedDeltaTime;
+    }
+
+    private void Update()
+    {
+        Vector2 vectorToSpawn = spawnPos - (Vector2)transform.position;
+        float distance = Mathf.Abs(vectorToSpawn.magnitude);
+        if (distance > range / 100.0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (firedByPlayer)
+        {
+            if (collision.transform.tag == "Enemy")
+            {
+                collision.transform.GetComponent<Monster>().OnHit(damage);
+                Destroy(gameObject);
+            }
+        }
+        else
+        {
+            if (collision.transform.tag == "Player")
+            {
+                collision.transform.GetComponent<Player.Player>().OnHit(damage);
+                Destroy(gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/ProjectileSpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileSpawner : MonoBehaviour
+{
+    public static ProjectileSpawner instance;
+    public GameObject prefab;
+
+    private void Start()
+    {
+        if (instance)
+        {
+            Destroy(instance.gameObject);
+        }
+
+        instance = this;
+        prefab.SetActive(false);
+    }
+
+    public void SpawnProjectile(Vector2 pos, int itemID, Vector2 direction, float damage, float speed, float range, bool firedByPlayer)
+    {
+        GameObject projectile = Instantiate(prefab);
+        projectile.GetComponentInChildren<SpriteRenderer>().sprite = ItemDatabase.instance.getSprite(itemID);
+        projectile.GetComponent<Projectile>().speed = speed;
+        projectile.GetComponent<Projectile>().direction = direction;
+        projectile.GetComponent<Projectile>().damage = damage;
+        projectile.GetComponent<Projectile>().range = range;
+        projectile.GetComponent<Projectile>().firedByPlayer = firedByPlayer;
+        projectile.transform.position = pos;
+        projectile.GetComponent<Rigidbody2D>().position = pos;
+        projectile.SetActive(true);
+    }
+}
